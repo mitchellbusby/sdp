5b31ecb618d789c4a831e3bc284391d3446b6a75
Mitchell Busby <mitchbusby@gmail.com>
2015-09-02 16:41:26 +1000

    Angular bootstrapped.

---
+++ app.js
@@ -0,0 +1,12 @@
+// App.js
+'use strict'
+
+angular.module('utsHelps', [
+	'ngRoute'
+	])
+.run(function() {
+	console.log("Angular initialised!");
+})
+.controller('ApplicationController', ['$scope', function($scope){
+
+}]);
---
+++ controllers/ExampleController/ExampleTestController.js
@@ -0,0 +1,16 @@
+'use strict';
+
+describe('utsHelps.example module', function() {
+
+  beforeEach(module('utsHelps.example'));
+
+  describe('example controller', function(){
+
+    it('should ....', inject(function($controller) {
+      //spec body
+      var exampleCtrl = $controller('exampleCtrl');
+      expect(exampleCtrl).toBeDefined();
+    }));
+
+  });
+});
