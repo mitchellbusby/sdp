6ba6e5f61f1691efb64e07ef44c84bbc98f81658
Tommaso Armstrong <tomm@tomma.so>
2015-09-28 13:20:06 +1300

    WIP booking model

--- services/helpsRestfulServices.js
+++ services/helpsRestfulServices.js
@@ -22,7 +22,7 @@ angular.module('helpsRestfulServices', ['utsHelps.constants'])
 	"APP_KEY":'123456',
 	"ACTIVITIES_URI":"/workshop",
 	"SEARCH_URI":"/search",
-	"BOOKINGS_URI": "workshop/booking"
+	"BOOKINGS_URI": "/workshop/booking"
 })
 .config(['$sceDelegateProvider', 'helps_endpoint_constants', function($sceDelegateProvider, helps_endpoint_constants) {
 	// Resolves the long wait for the OPTIONS pre flight request; very much a hack
@@ -256,37 +256,30 @@ angular.module('helpsRestfulServices', ['utsHelps.constants'])
 				params
 			);
 		}
-		//this.mergeActivities = function(newDataToMerge, existingData) {
-		//	if (newDataToMerge.IsSuccess) {
-		//		if (!existingData) {
-		//			var existingData = {};
-		//		}
-		//		for (var i=0; i<newDataToMerge["Results"].length; i++){
-		//			if (existingData.hasOwnProperty(newDataToMerge["Results"][i]["WorkShopSetID"])) {
-		//				// Append all workshop deets and whatnot into the workshop
-		//				existingData[newDataToMerge["Results"][i]["WorkShopSetID"]]["workshops"].push(newDataToMerge["Results"][i]);
-		//			}
-		//			else {
-		//				existingData[newDataToMerge["Results"][i]["WorkShopSetID"]] = {
-		//					"description":newDataToMerge["Results"][i]["description"],
-		//					"topic":newDataToMerge["Results"][i]["topic"],
-		//					"workshops":[newDataToMerge["Results"][i]]
-		//				};
-		//			}
-		//		}
-		//		return existingData;
-		//	}
-		//	else {
-		//		// Failed to correctly load data
-		//		console.log("Failed to correctly load data.");
-		//		$rootScope.$broadcast(ERR_BROADCASTS.API_ERROR, newDataToMerge.DisplayMessage);
-		//		return {};
-		//	}
-		//}
+
+		this.mergeBookings = function(newDataToMerge, existingData) {
+			if (newDataToMerge.IsSuccess) {
+
+				existingData = typeof existingData !== 'undefined' ? existingData : {};
+
+				for (var i=0; i<newDataToMerge["Results"].length; i++) {
+					existingData[newDataToMerge["Results"][i]["BookingId"]] = (newDataToMerge["Results"][i]);
+				}
+
+				return existingData;
+			}
+			else {
+				// Failed to correctly load data
+				console.log("Failed to correctly load data.");
+				$rootScope.$broadcast(ERR_BROADCASTS.API_ERROR, newDataToMerge.DisplayMessage);
+				return {};
+			}
+		}
+
 		this.onCreate = function() {
 			this.getBookings({"studentID":10953659}).then(function(result) {
-				console.log(result);
-				//scope.activities = scope.mergeActivities(result.data);
+				console.log(JSON.stringify(result));
+				scope.bookings = scope.mergeBookings(result.data);
 			});
 		};
 		this.onCreate();
