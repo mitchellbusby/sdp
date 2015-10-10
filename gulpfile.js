d6cfc4da0804ac2b278806407af20052f229e844
Mitchell Busby <mitchbusby@gmail.com>
2015-10-11 09:06:57 +1100

    Adds error logging to gulp

--- gulpfile.js
+++ gulpfile.js
@@ -12,6 +12,9 @@ var concat = require('gulp-concat');
 var sourcemaps = require('gulp-sourcemaps');
 // Dependency for minify
 var uglify = require('gulp-uglify');
+//Error logging
+var gutil = require('gulp-util');
+
 
 gulp.task('default', ['sass'], function(){
 
@@ -60,7 +63,7 @@ gulp.task('concat-js', function() {
 	return gulp.src(jsFiles, {base: './'})
 		.pipe(sourcemaps.init())
 			.pipe((concat('main.js')))
-				.pipe(uglify())
+				.pipe(uglify().on('error', gutil.log))
 					.pipe(sourcemaps.write())
 						.pipe(gulp.dest('.'));
 });
