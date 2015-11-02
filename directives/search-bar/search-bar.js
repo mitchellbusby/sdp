ff6e175f8b6aa65ad94e8e0c5cec0fec0abd6084
Shaen <shanebegovic@gmail.com>
2015-11-02 23:40:10 +1100

    Fix reload page filter bug

--- directives/search-bar/search-bar.js
+++ directives/search-bar/search-bar.js
@@ -8,7 +8,10 @@ angular.module('utsHelps.helpsSearchBar', [])
       var callbackFunction = $parse(attrs.callback);
       searchIn.on('keyup', function() {
         callbackFunction(scope, {query:searchIn[0].value});
-      })
+      });
+
+      // send a blank query on load, so that we clear the filter when accessing page.
+      setTimeout(function() { callbackFunction(scope, {query:""})}, 100);
     },
   }
 }]);
